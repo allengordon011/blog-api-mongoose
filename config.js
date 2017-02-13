68c8e4d7ee6ff1a22c3be767fcdd0172c16f5a48
trig010 <trig.code@gmail.com>
2017-02-13 15:50:45 -0600

    correcting typo

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 exports.DATABASE_URL = process.env.DATABASE_URL ||
                        global.DATABASE_URL ||
-                      'mongodb://mongodb://localhost/blog-api-mongoose';
+                      'mongodb://localhost/blog-api-mongoose';
 exports.PORT = process.env.PORT || 8080;
