35c9b88759068d568631a82fea6afafe4990eb11
trig010 <trig.code@gmail.com>
2017-02-13 15:39:06 -0600

    all endpoints working

---
+++ server.js
@@ -0,0 +1,140 @@
+const bodyParser = require('body-parser');
+const express = require('express');
+const mongoose = require('mongoose');
+
+mongoose.Promise = global.Promise;
+
+const {PORT, DATABASE_URL} = require('./config');
+const {Blogpost} = require('./models');
+
+const app = express();
+app.use(bodyParser.json());
+
+app.get('/posts', (req, res) => {
+    Blogpost.find().exec().then(blogposts => {
+        res.status(200).json({
+            blogposts: blogposts.map(blogpost => blogpost.apiRepr())
+        })
+    }).catch(err => {
+        console.error(err);
+        res.status(500).json({message: 'Internal server error'})
+    });
+});
+
+app.get('/posts/:id', (req, res) => {
+    Blogpost.findById(req.params.id)
+    .exec()
+    .then(blogpost => {
+        res.status(200).json(blogpost.apiRepr())})
+    .catch(err => {
+        console.error(err);
+        res.status(500).json({message: 'Internal server error'})
+    })
+});
+
+app.post('/posts', (req, res) => {
+    const requiredFields = ['title', 'content', 'author'];
+    for (let i=0; i<requiredFields.length; i++) {
+      const field = requiredFields[i];
+      if (!(field in req.body)) {
+        const message = `Missing \`${field}\` in request body`
+        console.error(message);
+        return res.status(400).send(message);
+      }
+    }
+
+    Blogpost.create({
+        title: req.body.title,
+        content: req.body.content,
+        author: {
+            firstName: req.body.author.firstName,
+            lastName: req.body.author.lastName,
+        },
+        created: new Date()
+    })
+    .then(
+        res.status(201).json(blogpost => blogpost.apiRepr())
+    )
+    .catch(err => {
+        console.error(err)
+        res.status(500).json({message: 'Internal server error'})
+    });
+});
+
+app.put('/posts/:id', (req, res) => {
+      if (!('id' in req.body)) {
+        const message = `Missing "id" in request body`
+        console.error(message);
+        return res.status(400).send(message);
+      }
+      const toUpdate = {};
+      const updateableFields = ['title', 'content', 'author'];
+      updateableFields.forEach(field => {
+        if (field in req.body) {
+          toUpdate[field] = req.body[field];
+        }
+      });
+
+      Blogpost
+        .findByIdAndUpdate(req.params.id, {$set: toUpdate})
+        .exec()
+        .then(blogpost => res.status(204).end())
+        .catch(err => res.status(500).json({message: 'Internal server error'}));
+});
+
+app.delete('/posts/:id', (req, res) => {
+    Blogpost.findByIdAndRemove(req.params.id)
+    .exec()
+    .then(blogpost => res.status(204).end())
+    .catch(err => res.status(500).json({message: 'Internal server error'}));
+});
+
+
+let server;
+
+// this function connects to our database, then starts the server
+function runServer(databaseUrl = DATABASE_URL, port = PORT) {
+
+    return new Promise((resolve, reject) => {
+        mongoose.connect(databaseUrl, err => {
+            if (err) {
+                return reject(err);
+            }
+            server = app.listen(port, () => {
+                console.log(`Your app is listening on port ${port}`);
+                resolve();
+            }).on('error', err => {
+                mongoose.disconnect();
+                reject(err);
+            });
+        });
+    });
+}
+
+// this function closes the server, and returns a promise. we'll
+// use it in our integration tests later.
+function closeServer() {
+    return mongoose.disconnect().then(() => {
+        return new Promise((resolve, reject) => {
+            console.log('Closing server');
+            server.close(err => {
+                if (err) {
+                    return reject(err);
+                }
+                resolve();
+            });
+        });
+    });
+}
+
+// if server.js is called directly (aka, with `node server.js`), this block
+// runs. but we also export the runServer command so other code (for instance, test code) can start the server as needed.
+if (require.main === module) {
+    runServer().catch(err => console.error(err));
+};
+
+module.exports = {
+    app,
+    runServer,
+    closeServer
+};
